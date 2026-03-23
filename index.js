5b977a15b77753577a8b7c5f5a7e816f2a7cc20a
gloriamich <gloria.mich@student.moringaschool.com>
2026-03-24 00:08:14 +0300

    completed login tracker

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0; 
+  const maxAttempts = 3;
+   return (inputPassword) =>{
+    if (attemptCount >= 3)
+      return "Account locked, too many attempts.";
+  
+  if (inputPassword === userInfo.password) {
+    attemptCount = 0; 
+    return "Successful Login";
+  } else {
+    attemptCount++;
+    if (attemptCount >= 3) {
+      return "Account locked. Too manny failed attempts.";
+    }
+  
+  
+    return `Incorrect  password. Attempts remaining: ${maxAttempts - attemptCount}`;
+  }
+  };
+   }
+  const account = createLoginTracker({ username: "Glo", password: "123"});
+
+  console.log(account("wrong"));
+  console.log(account("wrong"));
+  console.log(account("wrong"));
+  console.log(account("123"));
 
 
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+module.exports = createLoginTracker
